0adb4ebb5d82b4bf7e74a98ec10588b94d98210f
Kristen <kristen.ramaila@gmail.com>
2023-09-16 11:35:04 -0700

    final commit

---
+++ arrow.js
@@ -0,0 +1,5 @@
+/* Write an ES2015 Version */
+const double = arr => arr.map(val => val * 2);
+
+/* Replace ALL functions with arrow functions */
+const squareAndFindEvens = numbers => numbers.map(val => val **2).filter(square => square % 2 ===0)
